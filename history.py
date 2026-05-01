14725e95ec198e570da2a4fa2eb75f1bc1a75e33
serafinchen <serafina333@web.de>
2026-05-01 11:53:48 +0200

    gitignore

---
+++ history.py
@@ -0,0 +1,10 @@
+import sqlite3
+import shutil
+import datetime
+
+def chrome_time_to_datetime(chrome_time):
+      return datetime.datetime(1601, 1, 1) + datetime.timedelta(microseconds=chrome_time)
+
+
+if __name__ == "__main__":
+      pass
