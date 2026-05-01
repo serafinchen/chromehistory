cfbcee1b751fb7599ee1f8c47e0083004498cf34
serafinchen <serafina333@web.de>
2026-05-01 12:33:47 +0200

    saving in dict

--- history.py
+++ history.py
@@ -8,14 +8,33 @@ load_dotenv()
 def chrome_time_to_datetime(chrome_time):
       return datetime.datetime(1601, 1, 1) + datetime.timedelta(microseconds=chrome_time)
 
-
-if __name__ == "__main__":
+def sql_query(query):
       user = os.getenv("USER")
       historyFile = "C:\\Users\\"+user+"\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History"
 
       shutil.copy2(historyFile, "history_copy.db") 
       c = sqlite3.connect("history_copy.db") 
       cursor = c.cursor()
+      cursor.execute(query)
+
+      rows = cursor.fetchall()
+      columns = [col[0] for col in cursor.description]
+      result = []
+      for row in rows:
+            
+            row_dict = dict(zip(columns, row))
+
+            if "visit_time" in row_dict:
+                  row_dict["visit_time"] = str(
+                        chrome_time_to_datetime(row_dict["visit_time"])
+                  )
+
+            result.append(row_dict)      
+
+      return result
+
+
+if __name__ == "__main__":
 
       selectStatement = """ 
       SELECT visits.visit_time, urls.url, urls.title 
@@ -24,7 +43,5 @@ if __name__ == "__main__":
       ORDER BY visits.visit_time ASC 
       LIMIT 50; 
       """ 
-      for row in cursor.execute(selectStatement): 
-            time = chrome_time_to_datetime(row[0]) 
-            print (str(time)+"URL:"+str(row[1])+str(row[2])+"\n")
+      print(sql_query(selectStatement))
 
